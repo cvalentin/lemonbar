b3b8d3c39d8eb268ee7f3a8181e2ffa46de27b95
Thomas Dorsey <homelessicecube@gmail.com>
2016-03-13 16:50:07 -0700

    Added gorilla routing & two new pages

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+    "log"
     "fmt"
     "net/http"
+    "github.com/gorilla/mux"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+    fmt.Fprintf(w, "HELLO WORLD", r.URL.Path[1:])
+}
+
+func coolHandler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "BACKEND RULES")
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+    r := mux.NewRouter()
+    r.HandleFunc("/", handler)
+    r.HandleFunc("/profile", coolHandler)
+    r.HandleFunc("/events", coolHandler)
+
+    http.Handle("/", r)
+    log.Println("Listening...")
+    http.ListenAndServe(":3000", nil)
 }
