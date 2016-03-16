2704287265b0e189c64cdcbf40662699067bd962
Thomas Dorsey <homelessicecube@gmail.com>
2016-03-15 20:19:28 -0700

    Added more routes; left executable in to annoy Carlo

--- router.go
+++ router.go
@@ -11,28 +11,28 @@ import (
 func getRoutes() *mux.Router {
     r := mux.NewRouter()
 
-    r.HandleFunc("/profile", profilerHandler)
-    r.HandleFunc("/events", coolHandler)
+    r.HandleFunc("/group", staticPageHandler)
+    r.HandleFunc("/profile", staticPageHandler)
+    r.HandleFunc("/index", staticPageHandler)
+    r.HandleFunc("/library", staticPageHandler)
+    r.HandleFunc("/event", staticPageHandler)
 
-    staticHandler := http.StripPrefix("/static/", http.FileServer(http.Dir("./static/")))
-    r.PathPrefix("/static").Handler(staticHandler)
+    staticAssetsHandler := http.StripPrefix("/static/", http.FileServer(http.Dir("./static/")))
+    r.PathPrefix("/static").Handler(staticAssetsHandler)
 
-    r.HandleFunc("/", rootHandler)
+    r.HandleFunc("/", rootRedirect)
+
     return r
 }
 
-func profilerHandler(w http.ResponseWriter, r *http.Request) {
-    body, err := ioutil.ReadFile("views/profile.html")
+func staticPageHandler(w http.ResponseWriter, r *http.Request) {
+    body, err := ioutil.ReadFile("views" + r.URL.String() + ".html")
     if err != nil {
         log.Fatal(err)
     }
     fmt.Fprintf(w, "%s", string(body))
 }
 
-func coolHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "BACKEND RULES")
-}
-
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func rootRedirect(w http.ResponseWriter, r *http.Request) {
+    http.Redirect(w, r, "/index", 301)
 }
