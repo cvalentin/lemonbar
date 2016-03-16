79eea9497de18a210b7ef194da72a7043627478d
Carlo Valentin <me@carlovalentin.com>
2016-03-15 20:34:30 -0700

    Fix port to work on Heroku

--- main.go
+++ main.go
@@ -5,9 +5,19 @@ import (
     "net/http"
 )
 
+func getPort() string {
+        var port = os.Getenv("PORT")
+        // Set a default port if there is nothing in the environment
+        if port == "" {
+                port = "3000"
+                fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+        }
+        return ":" + port
+}
+
 func main() {
     http.Handle("/", getRoutes())
 
     log.Println("Listening...")
-    http.ListenAndServe(":3000", nil)
+    http.ListenAndServe(getPort(), nil)
 }
