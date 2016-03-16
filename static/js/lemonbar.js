f2fefb4d25b85ac9539f3deac46601540403ace2
ramirs <ramirs2015@gmail.com>
2016-03-15 20:40:20 -0700

    interactivity on library page, added jquery

---
+++ static/js/lemonbar.js
@@ -0,0 +1,16 @@
+(function(){
+
+$('.close').click(function(){
+	$(this.parentNode).remove();
+});
+
+$('.form-input').keydown(function(e){
+	if ( event.which == 13 ) {
+		$(".content-labels").append("<div class=\"content-label\">"+ $('.form-input').val() + "<span class=\"close\">x</span></div>");
+	}
+	$('.close').click(function(){
+		$(this.parentNode).remove();
+	});
+});
+
+})();
